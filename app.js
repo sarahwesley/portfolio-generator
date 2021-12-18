34a3223607a52d273e006d1c30d92d21987711ea
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-12-18 11:03:02 -0500

    inquirer added, question added

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Whatis your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
 
 
+// fs.writeFile('./index.html', pageHTML,  err => {
+//   if (err) throw new Error(err);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//   console.log('portfolio complete! checkout index.html to see the output!')
+// });
 
-  console.log('portfolio complete! checkout index.html to see the output!')
-})
+
+////////////////////////////////
 
 // const printProfileData = profileDataArr => {
 //   // This...
